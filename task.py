9ff99b9d1366f99d0ddd72e0693d373d8a89a677
Vladimir7337 <145162158+Vladimir7337@users.noreply.github.com>
2023-09-16 12:44:54 +0300

    task

---
+++ task.py
@@ -0,0 +1,21 @@
+a = float(input(f"введите a: ")) #вводим 3
+b = float(input(f"введите b: ")) #вводим 5
+c = float(input(f"введите c: ")) #вводим 10
+d = float(input(f"введите d: ")) #вводим 49
+e = float(input(f"введите e: ")) #вводим 0.5
+f = float(input(f"введите f: ")) #вводим -10
+k = float(input(f"введите k: ")) #вводим 15
+l = float(input(f"введите l: ")) #вводим 1
+m = float(input(f"введите m: ")) #вводим 100
+n = float(input(f"введите n: ")) #вводим 1
+o = float(input(f"введите o: ")) #вводим 3
+x = a * b * c * d ** e #числитель
+y = abs(f) * k * (float(l)/m) ** float(n)/o #знаменатель
+print(f"результат: {x/y}") #вычисление значения функции
+
+
+
+
+
+
+
